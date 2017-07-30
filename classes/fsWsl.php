bb09c039f932aaf9e5fff77a47f6948c50230801
robomono <ecampa20@gmail.com>
2017-07-30 10:39:53 -0700

    main build of rankings for sm and lg screens

--- classes/fsWsl.php
+++ classes/fsWsl.php
@@ -19,7 +19,7 @@ class WSL{
 		
 	public function calculateRankings(){
 		
-		$user_id = 107; //eventually get userr id using session
+		$user_id = 105; //eventually get userr id using session
 		$last_event = 6; //eventually get event using last event in status 2 / 3 / 4
 		$league_id = 1; //eventually get league id from users info
 				
@@ -80,12 +80,73 @@ class WSL{
 	
 	}
 	
+	public function getHeatResults(){
+		
+		//Get heat results
+		
+		$this->db_connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+	
+		if (!$this->db_connection->set_charset("utf8")) {$this->errors[] = $this->db_connection->error;}
+		
+		if (!$this->db_connection->connect_errno) {
+		
+			$sql = "SELECT event_id,round,heat,result,surfer_id FROM heats";
+					
+     		$result = $this->db_connection->query($sql);
+	
+			while($row = mysqli_fetch_array($result)){
+				//figure out the result and write as text
+
+				if($row['round']==1 || $row['round']==4){
+					if($row['result']==1){
+						$heats[$row['surfer_id']][$row['event_id']][$row['round']] = "won";
+						$heats[$row['surfer_id']][$row['event_id']][$row['round']+1] = "won";
+					}else{
+						$heats[$row['surfer_id']][$row['event_id']][$row['round']] = "relegated";
+					}
+					
+				}else{
+					if($row['result']==1){
+						$heats[$row['surfer_id']][$row['event_id']][$row['round']] = "won";
+					}else{
+						$heats[$row['surfer_id']][$row['event_id']][$row['round']] = "lost";
+					}
+					
+				}
+				
+			}
+		
+		}
+		
+		
+		return $heats;
+		
+	}
 	
 	public function getWslRankings(){
 		
+		$position[2] = 25;
+		$position[3] = 13;
+		$position[5] = 9;
+		$position[6] = 5;
+		$position[7] = 3;
+		$position[8] = 2;
+		
 		$scores = $this->calculateRankings();
+		$heats = $this->getHeatResults();
 		
 		$last_event = 6; //eventually get event using last event in status 2 / 3 / 4
+		$events[1]['name'] = "Quiksilver Pro Gold Coast";
+		$events[2]['name'] = "Drug Aware Margaret River Pro";
+		$events[3]['name'] = "Rip Curl Pro Bells Beach";
+		$events[4]['name'] = "Oi Rio Pro";
+		$events[5]['name'] = "Outerknown Fiji Pro";
+		$events[6]['name'] = "Corona Open J-Bay";
+		$events[7]['name'] = "Billabong Pro Tahiti";
+		$events[8]['name'] = "Hurley Pro at Trestles";
+		$events[9]['name'] = "Quiksilver Pro France";
+		$events[10]['name'] = "MEO Rip Curl Pro Portugal";
+		$events[11]['name'] = "Billabong Pipe Masters";
 		
 		//create ranking based on scores
 		foreach($scores as $sid=>$v1){
@@ -93,19 +154,22 @@ class WSL{
 		}
 		arsort($totals);
 		
-		$prevtot = 0;$ranking=0;
+		$prevtot = 0;$ranking=0;$oddcount = 0;
 		foreach($totals as $sid=>$total){
+			
+			if($oddcount % 2==0){$isodd="oddline";}else{$isodd="evenline";}
+			$oddcount++;
 			
 			//calculate ranking
 			if($prevtot==$total){}else{$ranking++;$prevtot=$total;}
 
 			//calculate availability
 			if($scores[$sid]['picked']==1){
-				$team = '<i style="font-size:14pt;margin-top:6px;" class="material-icons">check_circle</i>';
+				$team = '<i style="font-size:14pt;margin-top:3px;color:#00A633;" class="material-icons">check_circle</i>';
 			}elseif($scores[$sid]['avail']>0){
-				$team = '<i style="font-size:14pt;margin-top:6px;" class="material-icons">timelapse</i>';
+				$team = '<i style="font-size:14pt;margin-top:3px;color:#F29F05;" class="material-icons">timelapse</i>';
 			}else{
-				$team = '<i style="font-size:14pt;margin-top:6px;" class="material-icons">do_not_disturb_on</i>';
+				$team = '<i style="font-size:14pt;margin-top:3px;color:#D92525;" class="material-icons">do_not_disturb_on</i>';
 			}
 			
 			$name = $scores[$sid]['name'];
@@ -137,7 +201,7 @@ class WSL{
 			}
 			
 		$display.='
-							<div class="row align-center align-middle wslsurfer hide-for-small-only">
+							<div class="row align-center align-middle wslsurfer '.$isodd.' hide-for-small-only">
 								<div class="large-1 medium-1 columns" style="padding:0px;">
 									<div class="wslsurferrank">'.$ranking.'</div>
 									<div class="wslsurferteam">'.$team.'</div>
@@ -146,62 +210,22 @@ class WSL{
 								<div class="large-3 medium-4 columns wslsurfername">'.$name.'</div>
 								
 								<div class="large-6 medium-5 columns wslsurferresults">
-										<div class="row">
-											<div class="performanceresult event'.$stat[1].'">
-												<span data-tooltip aria-haspopup="true" class="has-tip" title="Quiksilver Pro Gold Coast">
-													'.$pos[1].'
-												</span>
-											</div>
-											<div class="performanceresult event'.$stat[2].'">
-												<span data-tooltip aria-haspopup="true" class="has-tip" title="Drug Aware Margaret River Pro">
-													'.$pos[2].'
-												</span>
-											</div>
-											<div class="performanceresult event'.$stat[3].'">
-												<span data-tooltip aria-haspopup="true" class="has-tip" title="Rip Curl Pro Bells Beach">
-													'.$pos[3].'
-												</span>
-											</div>
-											<div class="performanceresult event'.$stat[4].'">
-												<span data-tooltip aria-haspopup="true" class="has-tip" title="Oi Rio Pro">
-													'.$pos[4].'
-												</span>
-											</div>
-											<div class="performanceresult event'.$stat[5].'">
-												<span data-tooltip aria-haspopup="true" class="has-tip" title="Fiji Pro">
-													'.$pos[5].'
-												</span>
-											</div>
-											<div class="performanceresult event'.$stat[6].'">
-												<span data-tooltip aria-haspopup="true" class="has-tip" title="Corona Open J-Bay">
-													'.$pos[6].'
-												</span>
-											</div>
-											<div class="performanceresult event'.$stat[7].'">
-												<span data-tooltip aria-haspopup="true" class="has-tip" title="Billabong Pro Tahiti">
-													'.$pos[7].'
-												</span>
-											</div>
-											<div class="performanceresult event'.$stat[8].'">
-												<span data-tooltip aria-haspopup="true" class="has-tip" title="Hurley Pro at Trestles">
-													'.$pos[8].'
-												</span>
-											</div>
-											<div class="performanceresult event'.$stat[9].'">
-												<span data-tooltip aria-haspopup="true" class="has-tip" title="Quiksilver Pro France">
-													'.$pos[9].'
-												</span>
-											</div>
-											<div class="performanceresult event'.$stat[10].'">
-												<span data-tooltip aria-haspopup="true" class="has-tip" title="MEO Rip Curl Pro Portugal">
-													'.$pos[10].'
-												</span>
-											</div>
-											<div class="performanceresult event'.$stat[11].'">
-												<span data-tooltip aria-haspopup="true" class="has-tip" title="Billabong Pipe Masters">
-													'.$pos[11].'
-												</span>
-											</div>
+										<div class="row">';
+								
+										for($e=1;$e<=11;$e++){
+											
+											
+								$display.= "<div class='performanceresult event".$stat[$e]."'>
+											<span data-tooltip aria-haspopup='true' class='has-tip' title='".$events[$e]['name']."'>
+												".$pos[$e]."
+											</span>
+											</div>";
+											
+										}
+										
+								
+											
+										$display.= '
 										</div>
 								</div>
 								
@@ -212,7 +236,140 @@ class WSL{
 							</div>
 							';
 						
-		
+			$display.= '<div class="small-12 columns expandedsurfer"><div class="row align-center align-middle">
+						<div class="small-12 columns '.$isodd.'" style="padding-bottom:15px;">
+						<div class="row align-middle" style="margin-top:10px">';
+			
+			
+			$display.= '<div class="small-12 show-for-small-only columns performancelabel">EVENT PERFORMANCE</div>';
+			
+			for($e=1;$e<=$last_event;$e++){
+				
+				$display .= '<div class="large-3 medium-4 large-offset-1 medium-offset-1 columns performanceeventname hide-for-small-only">'.$events[$e]['name'].'</div>';
+				$display .= '<div class="large-4 small-1 columns performanceeventnumber show-for-small-only">'.$e.'</div>';
+				$display.= '<div class="large-7 medium-6 small-11 columns"><div class="row align-middle">';
+				
+				for($r=1;$r<=8;$r++){
+					
+					if($heats[$sid][$e][$r] == "won"){
+						if($r<8){
+							$display .= '<div class="performanceround performancewon">&nbsp;</div>';
+						}elseif($r==8){
+							$display .= '<div class="performanceround performancewon">WON</div>';
+						}
+					}elseif($heats[$sid][$e][$r] == "relegated"){
+						$display .= '<div class="performanceround performancerelegated">&nbsp;</div>';
+					}elseif($heats[$sid][$e][$r] == "lost"){
+						if($r<6){
+							$display .= '<div class="performanceround event'.$position[$r].'">RD'.$r.'</div>';
+						}elseif($r==6){
+							$display .= '<div class="performanceround event5">QF</div>';
+						}elseif($r==7){
+							$display .= '<div class="performanceround event3">SF</div>';
+						}elseif($r==8){
+							$display .= '<div class="performanceround event2">F</div>';
+						}
+						
+					}else{
+						$display .= '<div class="performanceround performanceout">&nbsp;</div>';
+					}
+					
+				}
+				
+				$display .= '</div></div>';
+				
+			}
+			
+			$display.= '</div></div></div></div>';
+			//END LARGE SCREEN BUILD
+			
+			//START SMALL SCREEN BUILD
+			$display.= '<div class="leagueleaderboard show-for-small-only '.$isodd.'">';
+			$display.= '<div class="row align-center align-middle sm-standings-row closedchevron" id="sm-lbu'.$sid.'">';
+			
+			$display.= '<div class="small-1 columns sm-leaderboard-rank">'.$ranking.'</div>';
+			
+			
+			if($scores[$sid]['picked']==1){
+				$display.= '<div class="small-1 columns sm-leaderboard-status">
+								<i class="material-icons" style="font-size:12pt;margin-top:3px;color:#00A633;margin-top:6px;">check_circle</i>
+							</div>';
+			}elseif($scores[$sid]['avail']>0){
+				$display.= '<div class="small-1 columns sm-leaderboard-status">
+								<i class="material-icons" style="font-size:12pt;margin-top:3px;color:#F29F05;margin-top:6px;">timelapse</i>
+							</div>';
+			}else{
+				$display.= '<div class="small-1 columns sm-leaderboard-status">
+								<i class="material-icons" style="font-size:12pt;margin-top:3px;color:#D92525;margin-top:6px;">do_not_disturb_on</i>
+							</div>';
+			}
+			
+			$display.= '<div class="small-5 columns sm-leaderboard-username" id="sm-lbu'.$sid.'n">'.$name.'</div>';
+			
+			$display.= '<div class="small-4 columns sm-leaderboard-total" id="sm-lbu'.$sid.'n">'.number_format($total).'</div>';
+			
+			$display.= '<div class="small-1 columns sm-leaderboard-chevron" id="sm-lbu'.$sid.'n">
+							<i class="material-icons" style="margin-top:6px;">keyboard_arrow_left</i>
+						</div>';
+				
+			$display.= '<div class="small-12 columns noselect expandedsurfer '.$isodd.'" id="show-sm-lbu'.$sid.'"><div class="row align-middle" style="padding-bottom:15px;">';
+			
+			$display.= '<div class="small-12 show-for-small-only columns performancelabel" style="margin-top:10px">EVENT RESULTS</div>';
+			
+			for($e=1;$e<=11;$e++){	
+				$display.= "<div class='performanceresult event".$stat[$e]."'>
+									<span data-tooltip aria-haspopup='true' class='has-tip' title='".$events[$e]['name']."'>
+										".$pos[$e]."
+									</span>
+								</div>";
+			}
+			
+			$display.= '<div class="small-12 columns">
+						<div class="row align-middle" style="margin-top:10px">
+						<div class="small-12 show-for-small-only columns performancelabel">EVENT PERFORMANCE</div>';
+						
+			for($e=1;$e<=$last_event;$e++){
+				$display.= '<div class="large-4 small-1 columns performanceeventnumber show-for-small-only">'.$e.'</div>';
+				$display.= '<div class="small-11 columns"><div class="row align-middle">';
+				
+				for($r=1;$r<=8;$r++){
+					
+					if($heats[$sid][$e][$r] == "won"){
+						if($r<8){
+							$display .= '<div class="performanceround performancewon">&nbsp;</div>';
+						}elseif($r==8){
+							$display .= '<div class="performanceround performancewon">WON</div>';
+						}
+					}elseif($heats[$sid][$e][$r] == "relegated"){
+						$display .= '<div class="performanceround performancerelegated">&nbsp;</div>';
+					}elseif($heats[$sid][$e][$r] == "lost"){
+						if($r<6){
+							$display .= '<div class="performanceround event'.$position[$r].'">RD'.$r.'</div>';
+						}elseif($r==6){
+							$display .= '<div class="performanceround event5">QF</div>';
+						}elseif($r==7){
+							$display .= '<div class="performanceround event3">SF</div>';
+						}elseif($r==8){
+							$display .= '<div class="performanceround event2">F</div>';
+						}
+						
+					}else{
+						$display .= '<div class="performanceround performanceout">&nbsp;</div>';
+					}
+					
+				}
+				
+				$display.= '</div></div>';
+			}
+						
+			$display.= '</div></div>';
+			
+			$display.= '</div></div>';
+			
+			$display.= '</div>';
+			
+			$display.= '</div></div>';
+			
 		}
 		
 		
